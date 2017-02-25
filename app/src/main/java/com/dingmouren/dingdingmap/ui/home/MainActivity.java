5b7b47b1da32a4f655799e4c5349e5857b57ba91
dingmouren <1341520108@qq.com>
2017-02-25 18:10:38 +0800

    显示地图

---
+++ app/src/main/java/com/dingmouren/dingdingmap/ui/home/MainActivity.java
@@ -0,0 +1,59 @@
+package com.dingmouren.dingdingmap.ui.home;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.amap.api.maps.AMap;
+import com.amap.api.maps.MapView;
+import com.dingmouren.dingdingmap.R;
+import com.dingmouren.dingdingmap.base.BaseActivity;
+
+import butterknife.BindView;
+
+public class MainActivity extends BaseActivity {
+    @BindView(R.id.mapview) MapView mMapView;
+    private AMap mAMap;
+
+    @Override
+    public int setLayoutId() {
+        return R.layout.activity_main;
+    }
+
+    @Override
+    public void initView() {
+        if (null == mAMap){
+            mAMap = mMapView.getMap();
+        }
+        mAMap.showMapText(true);
+        mAMap.setMapType(AMap.MAP_TYPE_NORMAL);
+    }
+
+    @Override
+    public void initData() {
+
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        mMapView.onResume();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        mMapView.onPause();
+    }
+
+    @Override
+    protected void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+        mMapView.onSaveInstanceState(outState);
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        mMapView.onDestroy();
+    }
+}
---
+++ app/src/main/java/com/dingmouren/dingdingmap/ui/mine/MineActivity.java
@@ -0,0 +1,8 @@
+package com.dingmouren.dingdingmap.ui.mine;
+
+/**
+ * Created by dingmouren on 2017/2/25.
+ */
+
+public class MineActivity {
+}
