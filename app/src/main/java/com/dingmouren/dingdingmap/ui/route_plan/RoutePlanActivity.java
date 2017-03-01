cb3b23c0ffa10e3022ab9590cbd232a5b5091d6c
dingmouren <1341520108@qq.com>
2017-03-01 20:16:00 +0800

    开始做路线规划

---
+++ app/src/main/java/com/dingmouren/dingdingmap/ui/route_plan/RoutePlanActivity.java
@@ -0,0 +1,27 @@
+package com.dingmouren.dingdingmap.ui.route_plan;
+
+import android.os.Bundle;
+
+import com.dingmouren.dingdingmap.R;
+import com.dingmouren.dingdingmap.base.BaseActivity;
+
+/**
+ * Created by dingmouren on 2017/3/1.
+ */
+
+public class RoutePlanActivity extends BaseActivity {
+    @Override
+    public int setLayoutId() {
+        return R.layout.activity_route_plan;
+    }
+
+    @Override
+    public void initView(Bundle savedInstanceState) {
+
+    }
+
+    @Override
+    public void initData() {
+
+    }
+}
