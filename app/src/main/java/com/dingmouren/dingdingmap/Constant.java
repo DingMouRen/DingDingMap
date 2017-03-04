a30c518afbb6700236cbc3490f3205624f6ea0f6
dingmouren <1341520108@qq.com>
2017-03-04 17:21:18 +0800

    删除无用的代码

--- app/src/main/java/com/dingmouren/dingdingmap/Constant.java
+++ app/src/main/java/com/dingmouren/dingdingmap/Constant.java
@@ -6,10 +6,4 @@ package com.dingmouren.dingdingmap;
 
 public class Constant {
     public static final String TRAFFIC_ENABLE = "traffic_enable";
-    public static final String POINT_START_NAME = "point_start_name";//起点
-    public static final String POINT_START_Latitude = "point_start_latitude";//起点
-    public static final String POINT_START_Longitude = "point_start_longitude";//起点
-    public static final String POINT_END_NAME = "point_end_name";//终点
-    public static final String POINT_END_Latitude = "point_end_latitude";//终点
-    public static final String POINT_END_Longitude = "point_end_longitude";//终点
 }
